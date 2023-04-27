4e9801687a1936f7d8a76f81f0a67cc3be645c3f
hp <hp@LAPTOP-CVLBEESM>
2023-04-26 20:56:04 -0500

    Agregar archivos de proyecto.

---
+++ Peliculas/Models/movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Peliculas.Models
+{
+    public class movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
